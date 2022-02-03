0e3a530edcac26d4d37da448935414e8afee119d
Leonardo Holanda <nardoso@gmail.com>
2022-02-03 11:14:03 -0300

    First class created

--- src/main/java/com/leonardoholanda/salesManagement/resources/CategoryResources.java
+++ src/main/java/com/leonardoholanda/salesManagement/resources/CategoryResources.java
@@ -1,16 +1,29 @@
 package com.leonardoholanda.salesManagement.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.leonardoholanda.salesManagement.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST is working";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "IT");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<Category>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 }
